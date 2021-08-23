fb63f42a6ae4d68f7ac13ab3ea0dad54e11027e4
Thijs Cornelissen <cornelissent@who.int>
2021-08-23 10:54:08 +0200

    Update shutils.sh

--- .github/scripts/shutils.sh
+++ .github/scripts/shutils.sh
@@ -9,11 +9,13 @@ installAptPackages() {
       # Ensure apt has been updated before any packages are installed.
       if [ -z "${APT_GET_UPDATED}" ]; then
         echo "Updating apt-get..."
-        #apt-get clean
-        #apt-get dist-upgrade -y
-        #apt-get upgrade -y
-        #apt-get clean
-        #apt-get update
+        apt-get clean
+        apt-get dist-upgrade -y
+        apt-get upgrade -y
+        apt autoremove
+        apt full-upgrade
+        apt-get clean
+        apt-get update
         export APT_GET_UPDATED=1
       fi
 
